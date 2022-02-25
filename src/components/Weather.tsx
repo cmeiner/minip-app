48b065b9f522ec91a86ae4cd50bae77ffd2f8235
miMeiner <christian.meiner@medieinstitutet.se>
2022-02-25 11:48:24 +0100

    Wind direction arrow incl fontawesome Fixes #11

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,11 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown} from "@fortawesome/free-solid-svg-icons";
 import { useState, useEffect } from "react";
 import { loadImage } from "../loadImage";
 import { WeatherFetcher } from "../WeatherFetcher";
+
 function Weather() {
 
 
     const [selectedCity, setSelectedCity] = useState('')
-    const [cityWeather, setCityWeather] = useState({weather: "Sunny", temperature: "20 C", wind: 1, name: 'Gothenburg', url: "./assets/sunny/sunny1.jpg"})
+    const [cityWeather, setCityWeather] = useState({weather: "Sunny", temperature: "20 C", wind: 1, name: 'Gothenburg', url: "./assets/sunny/sunny1.jpg", deg: 20})
     const [infoTemp, setTemp] = useState(true)
     const [selectedTemp, setSelectedTemp] = useState('')
 
@@ -27,7 +30,7 @@ function Weather() {
         <div>
             <form onSubmit={setWeather}>
             <input placeholder="City..." className="input-bordered input" required onChange={handleWeatherCity} value={selectedCity} type="text" />
-            <button className="btn block mx-auto my-2" type="submit">Ändra stad</button>
+            <button className="btn block mx-auto my-2" type="submit">Search City</button>
             </form>
             <div className="card w-96 bg-neutral shadow-xl mx-auto text-white mt-2">
                 <figure className="px-10 pt-10">
@@ -37,9 +40,9 @@ function Weather() {
                     <h2 className="card-title">{cityWeather.name}</h2>
                     <p>Weather: {cityWeather.weather}</p>
                     <p>Temperature: {cityWeather.temperature}</p>
-                    <p>Windspeed: {cityWeather.wind} m/s</p>
+                    <p>Windspeed: <FontAwesomeIcon style={{transform: `rotate(${cityWeather.deg}deg)`}} icon={faArrowDown}/> {cityWeather.wind} m/s</p>
                     <div className="card-actions">
-                        <button className="btn btn-ghost">Läs mer</button>
+                        <button className="btn btn-ghost">Read More</button>
                     </div>
                 </div>
             </div>
