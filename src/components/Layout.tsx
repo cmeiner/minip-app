8a22fe953c7bf2a3e8193a175a28057eb9997ab7
Prisberg <fille.risberg@live.se>
2022-03-01 09:51:45 +0100

    errorboundary

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ErrorBoundary from "./errorBoundary";
 import LocationWeather from "./LocationWeather";
 import Navbar from "./Navbar";
 import SearchWeather from "./SearchWeather";
@@ -7,13 +8,15 @@ function Layout() {
   return (
     <div>
 
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/"  element={ <LocationWeather /> } />
-        <Route path="/search"  element={ <SearchWeather /> } />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <ErrorBoundary>
+          <Routes>
+            <Route path="/" element={<LocationWeather />} />
+            <Route path="/search" element={<SearchWeather />} />
+          </Routes>
+        </ErrorBoundary>
+      </BrowserRouter>
     </div>
   );
 }
